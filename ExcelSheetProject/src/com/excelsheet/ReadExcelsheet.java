b506b0ed1784f727a164cb77caef03a5a27c7942
swanand.zade <swanandzade@hotmail.com>
2019-10-18 22:25:54 +0100

    Added two files for Excel data comparison

--- ExcelSheetProject/src/com/excelsheet/ReadExcelsheet.java
+++ ExcelSheetProject/src/com/excelsheet/ReadExcelsheet.java
@@ -33,5 +33,39 @@ public static void main(String[] args) throws BiffException, IOException {
 	}
 	System.out.println();
 	}
+
+	/**
+	 * Code need to complete by creating the new Class and putting class in ArrayList
+	 * Task To-do
+	 * Create Object where we need to store the value of Entity - Employee/Student/Anything
+	 * create ArrayList and store mutliple Object in ArrayList
+	 *
+	 */
+	for(int i=1; i< sheet.getRows(); i++){
+		Cell[] cellArray = sheet.getRow(i);
+		System.out.println("Cell 0: " +cellArray[0].getContents());
+		//skObj.setSrNo(cellArray[0].getContents());
+		System.out.println("Cell 1: " +cellArray[1].getContents());
+		System.out.println("Cell 2: " +cellArray[2].getContents());
+		System.out.println("Cell 3: " +cellArray[3].getContents());
+		System.out.println("Cell 4: " +cellArray[4].getContents());
+		System.out.println("Cell 5: " +cellArray[5].getContents());
+		System.out.println("Cell 6: " +cellArray[6].getContents());
+		System.out.println("Cell 7: " +cellArray[7].getContents());
+
+		System.out.println("========================================");
+		for(Cell cell: cellArray){
+			System.out.print(cell.getContents() + "------");
+
+
+
+		}
+		System.out.println();
+
+	}
+
+	/*End of the code*/
+
+
 }
 }
